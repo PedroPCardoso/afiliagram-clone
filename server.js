0a1ae0b739ffaa16bbf9f96c6f6e0f9fc8ac59ea
Pedro Cardoso <pedro.cardoso@absamhost.com.br>
2020-09-21 15:58:21 -0300

    test

--- server.js
+++ server.js
@@ -7,6 +7,17 @@ app.use(express.static("express"));
 // default URL for website
 app.use('/', function(req,res){
     res.sendFile(path.join(__dirname+'/express/index.html'));
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
     //__dirname : It will resolve to your project folder.
   });
 const server = http.createServer(app);
